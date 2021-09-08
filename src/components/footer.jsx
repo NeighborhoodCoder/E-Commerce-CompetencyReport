0c1a42765ae387517f294c92fc29b4f52e2018bb
NeighborhoodCoder <89759164+NeighborhoodCoder@users.noreply.github.com>
2021-09-08 01:35:36 -0400

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer bg-success text-light">
+      <h5>Tech Corner</h5>
+      <p>All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
